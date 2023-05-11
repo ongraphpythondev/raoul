823952ec113828c0a3a0f1c3b4f6453f71e3266b
pythondev1 <pythondev@ongraph.com>
2023-05-11 16:19:21 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+import os
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+import streamlit as st
+load_dotenv()
+openai_api=os.getenv("Open_ai_api")
+
+
+st.title("Issue Classifier")
+text=st.text_input("Text")
+
+
+if text:
+
+
+
+    # Prompt Template
+
+    template='''{email}
+        
+        In the above email find the catagory categorizes the customer query into predefined categories such as "billing inquiry," "technical issue," "product inquiry," etc., based on its content and keywords.
+
+        the output will be like below example
+
+        Example Category:["billing inquiry","technical issue","product inquiry"],Request:["what client wants now?"]
+        '''
+    print(openai_api)
+
+    categorization_template=PromptTemplate(
+        input_variables=["email"],
+        template=template,
+        validate_template=False
+    )
+    
+    llm_model=OpenAI(openai_api_key=openai_api,
+                     temperature=0)
+    category_chain=LLMChain(llm=llm_model,prompt=categorization_template,verbose=True)
+    
+    
+    output=category_chain.run(text)
+    
+    print("output",output)
+    st.write(output)
